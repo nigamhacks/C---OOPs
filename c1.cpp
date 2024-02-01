09eb3549758419b23f1b44d3ae464463ab711e4d
Your Name <sachinnigam9@live.com>
2024-02-01 20:53:20 +0530

    first commit

---
+++ c1.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+    
+    class Hero{
+    public:
+char name[100];
+int health;
+char level;
+ // public 
+ // private 
+ // protected  ~
+
+};
+
+
+
+
+int main(){
+
+Hero h1;
+cout<<"size " << sizeof(h1)<<endl;
+
+}
